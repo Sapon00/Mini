a8dde6e4af45253f72eef546d009761c35f04971
Sapon00 <shahadot623s@gmail.com>
2025-11-20 12:31:32 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "adminmention",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "SHAHADAT SAHU ",
+ credits: "Sapon Ahmed  ",
  description: "Bot will reply when someone tags any of the admins",
  commandCategory: "Other",
  usages: "@",
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61575698041722", "100001039692046", "61576394315037"].map(String);
+ const adminIDs = ["61584100463717", "61584100463717", "61584100463717"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
